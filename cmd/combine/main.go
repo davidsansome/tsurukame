fc850a4ed2eae99afc3743a4a6983d22daf259a1
David Sansome <me@davidsansome.com>
2017-11-23 21:24:41 +1100

    Add a combine command

---
+++ cmd/combine/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"github.com/golang/protobuf/proto"
+
+	pb "github.com/davidsansome/wk/proto"
+)
+
+var (
+	directory = flag.String("directory", "data", "Directory to read data files from")
+	out       = flag.String("out", "data.bin", "Output file")
+
+	order = binary.LittleEndian
+)
+
+func main() {
+	flag.Parse()
+
+	if err := Combine(); err != nil {
+		panic(err)
+	}
+
+}
+
+func Combine() error {
+	// List files.
+	files, err := ioutil.ReadDir(*directory)
+	if err != nil {
+		return err
+	}
+
+	// Read everything into memory.
+	all := make([][]byte, len(files))
+	for _, f := range files {
+		data, err := ioutil.ReadFile(path.Join(*directory, f.Name()))
+		if err != nil {
+			return err
+		}
+
+		var spb pb.Subject
+		if err := proto.Unmarshal(data, &spb); err != nil {
+			return err
+		}
+		id := spb.GetId()
+
+		// Remove fields we don't care about for the iOS app.
+		spb.DocumentUrl = nil
+		spb.Id = nil
+
+		data, err = proto.Marshal(&spb)
+		if err != nil {
+			return err
+		}
+
+		// Make space in the array for this ID.
+		for len(all) <= int(id) {
+			all = append(all, nil)
+		}
+		all[id] = data
+	}
+
+	fh, err := os.Create(*out)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	// Write the index.
+	binary.Write(fh, order, int32(len(all)))
+	offset := 4 + 4*len(all)
+	for _, d := range all {
+		binary.Write(fh, order, int32(offset))
+		offset += len(d)
+	}
+
+	// Write each encoded protobuf.
+	for _, d := range all {
+		fh.Write(d)
+	}
+
+	return nil
+}
