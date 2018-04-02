87f7bbd95d17313a11353448adfec53e33345a62
David Sansome <me@davidsansome.com>
2018-04-02 22:34:46 +1000

    Add Apache license

--- ios/DataLoader.m
+++ ios/DataLoader.m
@@ -1,10 +1,16 @@
-//
-//  DataLoader.m
-//  wk
-//
-//  Created by David Sansome on 23/11/17.
-//  Copyright © 2017 David Sansome. All rights reserved.
-//
+// Copyright 2018 David Sansome
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "DataLoader.h"
 
