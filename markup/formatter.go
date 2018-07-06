8d13b5ed50dad79801053390adf5cc0e2fef27d8
David Sansome <me@davidsansome.com>
2018-07-06 20:26:19 +1000

    Parse italic, bold and links

--- markup/formatter.go
+++ markup/formatter.go
@@ -25,8 +25,11 @@ import (
 var (
 	tagRE = regexp.MustCompile(
 		`(?U)` +
-			`([^\[]*)` +
-			`(?:\[(/?(?:vocabulary|reading|ja|kanji|radical))\])`)
+			`([^\[<]*)` +
+			`(?:[\[<]` +
+			`(/?(?:vocabulary|reading|ja|kanji|radical|b|em|i|strong|kan|a))` +
+			`(?: href="([^"]+)"[^>]*)?` +
+			`[\]>])`)
 )
 
 func FormatText(completeText string) []*pb.FormattedText {
@@ -35,33 +38,60 @@ func FormatText(completeText string) []*pb.FormattedText {
 	matches := tagRE.FindAllStringSubmatchIndex(completeText, -1)
 	lastIndex := 0
 	var formatStack []pb.FormattedText_Format
+	var linkUrlStack []string
 	for _, match := range matches {
 		lastIndex = match[1]
 		text := completeText[match[2]:match[3]]
 		nextTag := completeText[match[4]:match[5]]
+		var href string
+		if match[6] != -1 {
+			href = completeText[match[6]:match[7]]
+		}
 
 		// Add this text.
 		if len(text) != 0 {
 			formatStackCopy := make([]pb.FormattedText_Format, len(formatStack))
 			copy(formatStackCopy, formatStack)
-			ret = append(ret, &pb.FormattedText{Format: formatStackCopy, Text: proto.String(text)})
+
+			formattedText := &pb.FormattedText{Format: formatStackCopy, Text: proto.String(text)}
+			if len(linkUrlStack) > 0 {
+				formattedText.LinkUrl = proto.String(linkUrlStack[len(linkUrlStack)-1])
+			}
+			ret = append(ret, formattedText)
 		}
 
 		// Add the next format tag.
 		if nextTag[0] == '/' {
+			lastTag := formatStack[len(formatStack)-1]
 			formatStack = formatStack[0 : len(formatStack)-1]
+			if lastTag == pb.FormattedText_LINK {
+				linkUrlStack = linkUrlStack[0 : len(linkUrlStack)-1]
+			}
 		} else {
 			switch nextTag {
 			case "radical":
 				formatStack = append(formatStack, pb.FormattedText_RADICAL)
-			case "kanji":
-				formatStack = append(formatStack, pb.FormattedText_KANJI)
 			case "ja":
 				formatStack = append(formatStack, pb.FormattedText_JAPANESE)
 			case "reading":
 				formatStack = append(formatStack, pb.FormattedText_READING)
 			case "vocabulary":
 				formatStack = append(formatStack, pb.FormattedText_VOCABULARY)
+			case "i":
+				formatStack = append(formatStack, pb.FormattedText_ITALIC)
+			case "kanji":
+				fallthrough
+			case "kan":
+				formatStack = append(formatStack, pb.FormattedText_KANJI)
+			case "b":
+				fallthrough
+			case "em":
+				fallthrough
+			case "strong":
+				formatStack = append(formatStack, pb.FormattedText_BOLD)
+			case "a":
+				formatStack = append(formatStack, pb.FormattedText_LINK)
+				linkUrlStack = append(linkUrlStack, href)
 			}
 		}
 	}
