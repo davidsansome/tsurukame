e7c496fa21ee5b094fdff65c24c8f17865911a60
Henri Schmidt <henri@reschmi.de>
2018-10-13 22:28:12 +0200

    WIP: build own Cell for font-preview

---
+++ ios/Tables/TKMFontModelItem.m
@@ -0,0 +1,24 @@
+//
+//  TKMFontModelItem.m
+//  Tsurukame
+//
+//  Created by Henri on 13.10.18.
+//  Copyright © 2018 David Sansome. All rights reserved.
+//
+
+#import "TKMFontModelItem.h"
+
+@implementation TKMFontModelItem
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
