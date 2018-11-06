bddd37722402721a894614c38d3b9eb16256040e
David Sansome <me@davidsansome.com>
2018-11-06 21:08:12 +1100

    Add an option to automatically play audio

--- ios/UserDefaults.m
+++ ios/UserDefaults.m
@@ -72,6 +72,7 @@ DEFINE_BOOL(meaningFirst, setMeaningFirst, YES);
 DEFINE_BOOL(showAnswerImmediately, setShowAnswerImmediately, YES);
 DEFINE_BOOL(enableCheats, setEnableCheats, YES);
 
+DEFINE_BOOL(playAudioAutomatically, setPlayAudioAutomatically, NO);
 DEFINE_OBJECT(NSSet<NSString *>, installedAudioPackages, setInstalledAudioPackages);
 
 @end
