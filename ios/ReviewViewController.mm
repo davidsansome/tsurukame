193210733617bc78fda8c5442e22717e76370023
Andre Arko <andre@arko.net>
2019-04-04 23:08:57 -0700

    add keyboard shortcuts for cheating

--- ios/ReviewViewController.mm
+++ ios/ReviewViewController.mm
@@ -1001,7 +1001,7 @@ class AnimationContext {
                                       handler:^(UIAlertAction *_Nonnull action) {
                                         ReviewViewController *unsafeSelf = weakSelf;
                                         if (unsafeSelf) {
-                                          [unsafeSelf markAnswer:TKMOverrideAnswerCorrect];
+                                          [unsafeSelf markCorrect];
                                         }
                                       }]];
   [c addAction:[UIAlertAction actionWithTitle:@"Ask again later"
@@ -1009,7 +1009,7 @@ class AnimationContext {
                                       handler:^(UIAlertAction *_Nonnull action) {
                                         ReviewViewController *unsafeSelf = weakSelf;
                                         if (unsafeSelf) {
-                                          [unsafeSelf markAnswer:TKMAskAgainLater];
+                                          [unsafeSelf askAgain];
                                         }
                                       }]];
   if (_activeTaskType == kTKMTaskTypeMeaning) {
@@ -1019,7 +1019,6 @@ class AnimationContext {
                                           ReviewViewController *unsafeSelf = weakSelf;
                                           if (unsafeSelf) {
                                             [unsafeSelf addSynonym];
-                                            [unsafeSelf markAnswer:TKMOverrideAnswerCorrect];
                                           }
                                         }]];
   }
@@ -1027,6 +1026,14 @@ class AnimationContext {
                                         style:UIAlertActionStyleCancel
                                       handler:nil]];
   [self presentViewController:c animated:YES completion:nil];
+}
+
+- (void)markCorrect {
+  [self markAnswer:TKMOverrideAnswerCorrect];
+}
+
+- (void)askAgain {
+  [self markAnswer:TKMAskAgainLater];
 }
 
 - (void)addSynonym {
@@ -1037,6 +1044,7 @@ class AnimationContext {
   }
   [_activeStudyMaterials.meaningSynonymsArray addObject:_answerField.text];
   [_services.localCachingClient updateStudyMaterial:_activeStudyMaterials];
+  [self markAnswer:TKMOverrideAnswerCorrect];
 }
 
 #pragma mark - TKMSubjectDelegate
@@ -1052,18 +1060,36 @@ class AnimationContext {
 }
 
 - (NSArray<UIKeyCommand *> *)keyCommands {
+  if (_subjectDetailsView.hidden) { return @[]; }
+
   return @[
     [UIKeyCommand keyCommandWithInput:@"\r"
-                       modifierFlags:0
-                              action:@selector(enterKeyPressed)],
+                        modifierFlags:0
+                               action:@selector(enterKeyPressed)
+                 discoverabilityTitle:@"Continue"],
     [UIKeyCommand keyCommandWithInput:@" "
-                       modifierFlags:0
-                              action:@selector(playAudio)]
+                        modifierFlags:0
+                               action:@selector(playAudio)
+                 discoverabilityTitle:@"Play reading"],
+    [UIKeyCommand keyCommandWithInput:@"a"
+                        modifierFlags:0
+                               action:@selector(askAgain)
+                 discoverabilityTitle:@"Ask again later"],
+    [UIKeyCommand keyCommandWithInput:@"c"
+                        modifierFlags:0
+                               action:@selector(markCorrect)
+                 discoverabilityTitle:@"Mark correct"],
+    [UIKeyCommand keyCommandWithInput:@"s"
+                        modifierFlags:0
+                               action:@selector(addSynonym)
+                 discoverabilityTitle:@"Add as synonym"]
   ];
 }
 
 - (void)playAudio {
-  [_subjectDetailsView playAudio];
+  if (!_subjectDetailsView.hidden) {
+    [_subjectDetailsView playAudio];
+  }
 }
 
 @end
