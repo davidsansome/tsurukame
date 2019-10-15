9f7b139b66d279507e66a2e66cbd5846bd5d61c1
Andre Arko <andre@arko.net>
2019-10-14 21:58:38 -0700

    Add return shortcut to continue studying

--- ios/MainViewController.m
+++ ios/MainViewController.m
@@ -534,6 +534,23 @@ static void SetTableViewCellCount(UITableViewCell *cell, int count) {
 
   NSMutableArray<UIKeyCommand *> *keyCommands = [NSMutableArray array];
 
+  // Press return to keep studying, first lessons then reviews
+  if (lessons && !reviews) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"\r"
+                         modifierFlags:0
+                                action:@selector(startLessons)
+                  discoverabilityTitle:@"Continue lessons"]
+     ];
+  } else if (reviews) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"\r"
+                         modifierFlags:0
+                                action:@selector(startReviews)
+                  discoverabilityTitle:@"Continue reviews"]
+     ];
+  }
+
   // Command L to start lessons, if any
   if (lessons) {
     [keyCommands addObject:
