c24e0a285a506897a1bbaac80b345ced006fdd35
André Arko <andre@arko.net>
2019-10-31 22:07:39 -0700

    Study shortcut improvements (#185)
    * Disable lesson/review shortcuts when none available
    
    * Add return shortcut to continue studying

--- ios/MainViewController.m
+++ ios/MainViewController.m
@@ -529,16 +529,48 @@ static void SetTableViewCellCount(UITableViewCell *cell, int count) {
 }
 
 - (NSArray<UIKeyCommand *> *)keyCommands {
-  return @[
-    [UIKeyCommand keyCommandWithInput:@"r"
-                        modifierFlags:UIKeyModifierCommand
-                               action:@selector(startReviews)
-                 discoverabilityTitle:@"Start reviews"],
-    [UIKeyCommand keyCommandWithInput:@"l"
-                        modifierFlags:UIKeyModifierCommand
-                               action:@selector(startLessons)
-                 discoverabilityTitle:@"Start lessons"]
-  ];
+  BOOL lessons = self.lessonsCell.userInteractionEnabled;
+  BOOL reviews = self.reviewsCell.userInteractionEnabled;
+
+  NSMutableArray<UIKeyCommand *> *keyCommands = [NSMutableArray array];
+
+  // Press return to keep studying, first lessons then reviews
+  if (lessons && !reviews) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"\r"
+                         modifierFlags:0
+                                action:@selector(startLessons)
+                  discoverabilityTitle:@"Continue lessons"]
+     ];
+  } else if (reviews) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"\r"
+                         modifierFlags:0
+                                action:@selector(startReviews)
+                  discoverabilityTitle:@"Continue reviews"]
+     ];
+  }
+
+  // Command L to start lessons, if any
+  if (lessons) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"l"
+                         modifierFlags:UIKeyModifierCommand
+                                action:@selector(startLessons)
+                  discoverabilityTitle:@"Start lessons"]
+     ];
+  }
+
+  // Command R to start reviews, if any
+  if (reviews) {
+    [keyCommands addObject:
+     [UIKeyCommand keyCommandWithInput:@"r"
+                         modifierFlags:UIKeyModifierCommand
+                                action:@selector(startReviews)
+                  discoverabilityTitle:@"Start reviews"]];
+  }
+
+  return keyCommands;
 }
 
 @end
