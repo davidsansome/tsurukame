4a086f2ad446b71f8ab338d06a14692652b281f2
David Sansome <me@davidsansome.com>
2020-01-19 18:18:56 +1300

    Use isEqual to compare strings rather than ==
    Fixes #272

--- ios/SearchResultViewController.m
+++ ios/SearchResultViewController.m
@@ -139,7 +139,8 @@ static bool SubjectMatchesQueryExactly(TKMSubject *subject, NSString *query, NSS
 
     dispatch_async(dispatch_get_main_queue(), ^{
       // If the query text changed since we started, don't update the list.
-      if (query != [searchController.searchBar.text lowercaseString]) {
+      NSString *newQuery = [searchController.searchBar.text lowercaseString];
+      if (![query isEqual:newQuery]) {
         return;
       }
       
