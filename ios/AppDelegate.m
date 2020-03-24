c1345e676ac5af786f71197039cbc7199e5e11f7
Bitrise <servicez+bitrise@pushd.com>
2020-03-24 10:24:45 -0700

    Review Feedback: Use cached counts

--- ios/AppDelegate.m
+++ ios/AppDelegate.m
@@ -65,25 +65,17 @@
 - (BOOL)application:(UIApplication *)application
     willContinueUserActivityWithType:(NSString *)userActivityType {
   if ([userActivityType isEqual:SiriShortcutHelper.ShortcutTypeReviews]) {
-    MainViewController *mainVC = [self findMainViewController];
-    NSArray<TKMAssignment *> *assignments = [_services.localCachingClient getAllAssignments];
-    NSArray<ReviewItem *> *items =
-        [ReviewItem assignmentsReadyForReview:assignments
-                                   dataLoader:_services.dataLoader
-                           localCachingClient:_services.localCachingClient];
-    if (items.count > 0) {
+    if (_services.localCachingClient.availableReviewCount > 0) {
       // If the user has 0 reviews proceed to the main view controller. If they have
       // 1+ reviews then launch directly into reviews.
+      MainViewController *mainVC = [self findMainViewController];
       [mainVC performSegueWithIdentifier:@"startReviews" sender:nil];
     }
   } else if ([userActivityType isEqual:SiriShortcutHelper.ShortcutTypeLessons]) {
-    MainViewController *mainVC = [self findMainViewController];
-    NSArray<TKMAssignment *> *assignments = [_services.localCachingClient getAllAssignments];
-    NSArray<ReviewItem *> *items = [ReviewItem assignmentsReadyForLesson:assignments
-                                                              dataLoader:_services.dataLoader];
-    if (items.count > 0) {
+    if (_services.localCachingClient.availableLessonCount > 0) {
       // If the user has 0 lessons proceed to the main view controller. If they have
       // 1+ lessons pending then launch directly into lessons.
+      MainViewController *mainVC = [self findMainViewController];
       [mainVC performSegueWithIdentifier:@"startLessons" sender:nil];
     }
   }
