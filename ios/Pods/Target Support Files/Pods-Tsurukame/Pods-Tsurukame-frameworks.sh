591c12e141047f7b01e8a1dfb53c0609321ea5f1
Matthew Benedict <MBenedict2004@gmail.com>
2020-08-03 09:21:25 -0500

    Convert TKMOfflineAudioViewController

--- ios/Pods/Target Support Files/Pods-Tsurukame/Pods-Tsurukame-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-Tsurukame/Pods-Tsurukame-frameworks.sh
@@ -202,7 +202,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Reachability/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Light-Untar/Light_Untar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MMDrawerController/MMDrawerController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -211,7 +210,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Reachability/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Light-Untar/Light_Untar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MMDrawerController/MMDrawerController.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
