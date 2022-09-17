2ca4a5b961d1ef2ad4dd378a39f9d12d6b176c7c
David Sansome <me@davidsansome.com>
2022-09-17 15:53:15 +1000

    Revert "Don't animate the fading question label when the keyboard size is changing"
    This reverts commit 73baa1d0e2906f8a717a6ad9d685d6435ac9ce0a.

--- ios/ReviewViewController.swift
+++ ios/ReviewViewController.swift
@@ -109,18 +109,7 @@ private class AnimationContext {
   let cheats: Bool
   let subjectDetailsViewShown: Bool
 
-  private struct FadingLabel {
-    let original: UILabel
-    let copy: UILabel
-
-    // Make copies of the original dimensions in case they're modified between when they're added
-    // and when they're animated.
-    let originalFrame: CGRect
-    let originalBounds: CGRect
-    let originalCenter: CGPoint
-  }
-
-  private var fadingLabels = [FadingLabel]()
+  private var fadingLabels = [(UILabel, UILabel)]()
 
   init(cheats: Bool, subjectDetailsViewShown: Bool) {
     self.cheats = cheats
@@ -130,32 +119,30 @@ private class AnimationContext {
   func addFadingLabel(original: UILabel) {
     let copy = copyLabel(original)
     original.alpha = 0.0
-    fadingLabels.append(FadingLabel(original: original, copy: copy, originalFrame: original.frame,
-                                    originalBounds: original.bounds,
-                                    originalCenter: original.center))
+    fadingLabels.append((original, copy))
   }
 
   func animateFadingLabels() {
-    for l in fadingLabels {
-      l.original.alpha = 1.0
-      switch l.original.textAlignment {
+    for (original, copy) in fadingLabels {
+      original.alpha = 1.0
+      switch original.textAlignment {
       case NSTextAlignment.natural:
         fallthrough
       case NSTextAlignment.left:
-        l.copy.center = CGPoint(x: l.originalFrame.minX + l.copy.frame.size.width / 2,
-                                y: l.originalFrame.minY + l.copy.frame.size.height / 2)
+        copy.center = CGPoint(x: original.frame.minX + copy.frame.size.width / 2,
+                              y: original.frame.minY + copy.frame.size.height / 2)
       default:
-        l.copy.center = l.originalCenter
-        l.copy.bounds = l.originalBounds
+        copy.center = original.center
+        copy.bounds = original.bounds
       }
-      l.copy.transform = l.original.transform
-      l.copy.alpha = 0.0
+      copy.transform = original.transform
+      copy.alpha = 0.0
     }
   }
 
   deinit {
-    for l in fadingLabels {
-      l.copy.removeFromSuperview()
+    for (_, copy) in fadingLabels {
+      copy.removeFromSuperview()
     }
   }
 }
@@ -413,7 +400,7 @@ class ReviewViewController: UIViewController, UITextFieldDelegate, SubjectDelega
     // to avoid moving the question label by offsetting its bottom constraint by the same amount the
     // keyboard moved.
     if let previousKeyboardInsetHeight = previousKeyboardInsetHeight,
-       abs(previousKeyboardInsetHeight - insetHeight) <= kSmallKeyboardHeightChange {
+       abs(insetHeight - previousKeyboardInsetHeight) <= kSmallKeyboardHeightChange {
       questionLabelBottomConstraint.constant = previousKeyboardInsetHeight - insetHeight
     } else {
       questionLabelBottomConstraint.constant = 0
