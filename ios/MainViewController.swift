708b4efca0e2b7628f826695f28b5b402378c6d5
Michael Babienco <mbabienco@southern.edu>
2023-12-28 18:31:54 +0900

    Don't force SRSStageCategoryItem to have indicator

--- ios/MainViewController.swift
+++ ios/MainViewController.swift
@@ -232,7 +232,8 @@ class MainViewController: UIViewController, LoginViewControllerDelegate,
     model.add(section: "All levels")
     for category in SRSStageCategory.apprentice ... SRSStageCategory.burned {
       let count = services.localCachingClient.srsCategoryCounts[category.rawValue]
-      let item = SRSStageCategoryItem(stageCategory: category, count: Int(count))
+      let item = SRSStageCategoryItem(stageCategory: category, count: Int(count),
+                                      accessoryType: count > 0 ? .disclosureIndicator : .none)
       if count > 0 {
         item.tapHandler = {
           self.selectedSrsStageCategory = category
