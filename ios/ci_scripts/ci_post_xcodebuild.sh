ed0c47f7b97deb57b1c3b014fbe40000417b0173
David Sansome <me@davidsansome.com>
2024-04-02 16:49:38 +0800

    Oops - fix a typo in the post build script

--- ios/ci_scripts/ci_post_xcodebuild.sh
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,5 @@
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
-  git fetch --deepen 3 && git log -3 --pretty=format:"- %s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-AU.txt
+  git fetch --deepen 3 && git log -3 --pretty=format:"- %s" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-AU.txt
 fi
