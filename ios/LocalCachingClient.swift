efd1015530459c7970aa9c7122169a375e9719f1
David Sansome <me@davidsansome.com>
2024-06-30 18:29:42 +0900

    Fix a possible crash when viewing lists of items

--- ios/LocalCachingClient.swift
+++ ios/LocalCachingClient.swift
@@ -594,7 +594,9 @@ private func postNotificationOnMainQueue(_ notification: Notification.Name) {
       "LEFT JOIN assignments AS a " +
       "ON p.id = a.subject_id " +
       "WHERE srs_stage >= \(category.firstSrsStage.rawValue) AND srs_stage <= \(category.lastSrsStage.rawValue)") {
-      ret.append(cursor.proto(forColumnIndex: 0)!)
+      if let pb: TKMAssignment = cursor.proto(forColumnIndex: 0) {
+        ret.append(pb)
+      }
     }
     return ret
   }
