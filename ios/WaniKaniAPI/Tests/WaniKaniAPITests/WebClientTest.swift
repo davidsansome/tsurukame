b849bab4dd2a6bc9eedc59e3dd862ab6157c8243
David Sansome <me@davidsansome.com>
2024-12-13 19:43:12 +1100

    Fix the rest of the API and web client tests

--- ios/WaniKaniAPI/Tests/WaniKaniAPITests/WebClientTest.swift
+++ ios/WaniKaniAPI/Tests/WaniKaniAPITests/WebClientTest.swift
@@ -20,11 +20,13 @@ import XCTest
 class WebClientTest: XCTestCase {
   var client: WaniKaniWebClient!
 
+  var loggedOutCsrfTokenResponseData: Data!
+  var loggedInCsrfTokenResponseData: Data!
+
   var getLoginPage: StubRequest!
   var postLoginPage: StubRequest!
   var getDashboardPage: StubRequest!
   var getApiToken: StubRequest!
-  var getEmailAddress: StubRequest!
   var createApiTokenGet: StubRequest!
   var createApiToken: StubRequest!
 
@@ -32,17 +34,22 @@ class WebClientTest: XCTestCase {
     client = WaniKaniWebClient()
     Hippolyte.shared.start()
 
+    loggedOutCsrfTokenResponseData = """
+      <meta name="csrf-token" content="blahblah"/>
+    """.data(using: .utf8)
+    loggedInCsrfTokenResponseData = """
+      <meta name="csrf-token" content="foobar123"/>
+    """.data(using: .utf8)
+
     getLoginPage = StubRequest(method: .GET, url: URL(string: "https://www.wanikani.com/login")!)
     getLoginPage.response.headers["Set-Cookie"] =
       "_wanikani_session=sessioncookie1; path=/; secure; HttpOnly"
-    getLoginPage.response.body = """
-      <meta name="csrf-token" content="blahblah"/>
-    """.data(using: .utf8)
+    getLoginPage.response.body = loggedOutCsrfTokenResponseData
 
     postLoginPage = StubRequest(method: .POST, url: URL(string: "https://www.wanikani.com/login")!)
     postLoginPage.bodyMatcher =
-      StringMatcher(string: "user%5Blogin%5D=foo&user%5Bpassword%5D=bar&user%5Bremember_me%5D=0&" +
-        "authenticity_token=blahblah&utf8=%E2%9C%93")
+      StringMatcher(string: "user%5Bemail%5D=foo%40example.com&user%5Bpassword%5D=bar&user%5Bremember_me%5D=0&" +
+        "authenticity_token=blahblah")
     postLoginPage.setHeader(key: "Cookie", value: "_wanikani_session=sessioncookie1")
     postLoginPage.response.statusCode = 302
     postLoginPage.response.headers["Set-Cookie"] =
@@ -66,32 +73,27 @@ class WebClientTest: XCTestCase {
         </td>
     """.data(using: .utf8)
 
-    getEmailAddress = StubRequest(method: .GET,
-                                  url: URL(string: "https://www.wanikani.com/settings/account")!)
-    getEmailAddress.setHeader(key: "Cookie", value: "_wanikani_session=sessioncookie2")
-    getEmailAddress.response.body = """
-      <input maxlength="255" placeholder="hello@wanikani.com" size="255" type="text"
-             value="email@example.com" name="user[email]" id="user_email"/>
-    """.data(using: .utf8)
-
     createApiTokenGet = StubRequest(method: .GET, url:
       URL(string: "https://www.wanikani.com/settings/personal_access_tokens")!)
-    createApiTokenGet.response.body = """
-      <meta name="csrf-token" content="foobar123"/>
-    """.data(using: .utf8)
+    createApiTokenGet.response.body = loggedInCsrfTokenResponseData
 
     createApiToken = StubRequest(method: .POST, url:
       URL(string: "https://www.wanikani.com/settings/personal_access_tokens")!)
     createApiToken.setHeader(key: "Cookie", value: "_wanikani_session=sessioncookie2")
     createApiToken
-      .bodyMatcher = StringMatcher(string: "personal_access_token%5Bdescription%5D=Tsurukame&" +
-        "personal_access_token%5Bpermissions%5D%5Bassignments%5D%5Bstart%5D=1&" +
-        "personal_access_token%5Bpermissions%5D%5Breviews%5D%5Bcreate%5D=1&" +
-        "personal_access_token%5Bpermissions%5D%5Breviews%5D%5Bupdate%5D=1&" +
-        "personal_access_token%5Bpermissions%5D%5Bstudy_materials%5D%5Bcreate%5D=1&" +
-        "personal_access_token%5Bpermissions%5D%5Bstudy_materials%5D%5Bupdate%5D=1&" +
+      .bodyMatcher = StringMatcher(string:
+        "description=Tsurukame&" +
+        "permissions%5Bassignments%5D%5Bstart%5D=0&" +
+        "permissions%5Bassignments%5D%5Bstart%5D=1&" +
+        "permissions%5Breviews%5D%5Bcreate%5D=0&" +
+        "permissions%5Breviews%5D%5Bcreate%5D=1&" +
+        "permissions%5Bstudy_materials%5D%5Bcreate%5D=0&" +
+        "permissions%5Bstudy_materials%5D%5Bcreate%5D=1&" +
+        "permissions%5Bstudy_materials%5D%5Bupdate%5D=0&" +
+        "permissions%5Bstudy_materials%5D%5Bupdate%5D=1&" +
+        "permissions%5Buser%5D%5Bupdate%5D=0&" +
         "authenticity_token=foobar123&" +
-        "utf8=%E2%9C%93")
+        "button=")
     createApiToken.response.body = """
       <tr data-id="personal-access-token__e03af9dd-aaa9-4a17-921e-3a29315767ec">
         <td class="personal-access-token-description">
@@ -112,12 +114,10 @@ class WebClientTest: XCTestCase {
     Hippolyte.shared.add(stubbedRequest: postLoginPage)
     Hippolyte.shared.add(stubbedRequest: getDashboardPage)
     Hippolyte.shared.add(stubbedRequest: getApiToken)
-    Hippolyte.shared.add(stubbedRequest: getEmailAddress)
 
-    if let response = waitForPromise(client.login(username: "foo", password: "bar")) {
+    if let response = waitForPromise(client.login(email: "foo@example.com", password: "bar")) {
       XCTAssertEqual(response.cookie, "sessioncookie2")
       XCTAssertEqual(response.apiToken, "00112233-4455-6677-8899-aabbccddeeff")
-      XCTAssertEqual(response.emailAddress, "email@example.com")
     }
   }
 
@@ -125,7 +125,7 @@ class WebClientTest: XCTestCase {
     getLoginPage.response.body = Data()
     Hippolyte.shared.add(stubbedRequest: getLoginPage)
 
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
+    if let error = waitForError(client.login(email: "foo@example.com", password: "bar")) {
       guard let webClientError = error as? WaniKaniWebClientError else {
         XCTFail("Bad error type: " + error.localizedDescription)
         return
@@ -138,7 +138,7 @@ class WebClientTest: XCTestCase {
     getLoginPage.response.headers.removeValue(forKey: "Set-Cookie")
     Hippolyte.shared.add(stubbedRequest: getLoginPage)
 
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
+    if let error = waitForError(client.login(email: "foo@example.com", password: "bar")) {
       guard let webClientError = error as? WaniKaniWebClientError else {
         XCTFail("Bad error type: " + error.localizedDescription)
         return
@@ -154,7 +154,7 @@ class WebClientTest: XCTestCase {
     Hippolyte.shared.add(stubbedRequest: postLoginPage)
     Hippolyte.shared.add(stubbedRequest: getDashboardPage)
 
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
+    if let error = waitForError(client.login(email: "foo@example.com", password: "bar")) {
       guard let webClientError = error as? WaniKaniWebClientError else {
         XCTFail("Bad error type: " + error.localizedDescription)
         return
@@ -163,75 +163,37 @@ class WebClientTest: XCTestCase {
     }
   }
 
-  func testBadRedirectUrl() {
-    postLoginPage.response.headers["Location"] = "https://example.com"
-
-    let badRedirect = StubRequest(method: .GET, url: URL(string: "https://example.com")!)
-
-    Hippolyte.shared.add(stubbedRequest: getLoginPage)
-    Hippolyte.shared.add(stubbedRequest: postLoginPage)
-    Hippolyte.shared.add(stubbedRequest: badRedirect)
-
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
-      guard let webClientError = error as? WaniKaniWebClientError else {
-        XCTFail("Bad error type: " + error.localizedDescription)
-        return
-      }
-      XCTAssertEqual(webClientError, WaniKaniWebClientError.unknown)
-    }
-  }
-
   func testCreateNewApiToken() {
-    getApiToken.response.body = Data()
+    getApiToken.response.body = loggedInCsrfTokenResponseData
     Hippolyte.shared.add(stubbedRequest: getLoginPage)
     Hippolyte.shared.add(stubbedRequest: postLoginPage)
     Hippolyte.shared.add(stubbedRequest: getDashboardPage)
     Hippolyte.shared.add(stubbedRequest: getApiToken)
-    Hippolyte.shared.add(stubbedRequest: getEmailAddress)
     Hippolyte.shared.add(stubbedRequest: createApiTokenGet)
     Hippolyte.shared.add(stubbedRequest: createApiToken)
 
-    if let response = waitForPromise(client.login(username: "foo", password: "bar")) {
+    if let response = waitForPromise(client.login(email: "foo@example.com", password: "bar")) {
       XCTAssertEqual(response.cookie, "sessioncookie2")
       XCTAssertEqual(response.apiToken, "e03af9dd-aaa9-4a17-921e-3a29315767ec")
-      XCTAssertEqual(response.emailAddress, "email@example.com")
     }
   }
 
   func testCreateNewApiTokenFailed() {
-    getApiToken.response.body = Data()
-    createApiToken.response.body = Data()
+    getApiToken.response.body = loggedInCsrfTokenResponseData
+    createApiToken.response.body = loggedInCsrfTokenResponseData
     Hippolyte.shared.add(stubbedRequest: getLoginPage)
     Hippolyte.shared.add(stubbedRequest: postLoginPage)
     Hippolyte.shared.add(stubbedRequest: getDashboardPage)
     Hippolyte.shared.add(stubbedRequest: getApiToken)
-    Hippolyte.shared.add(stubbedRequest: getEmailAddress)
     Hippolyte.shared.add(stubbedRequest: createApiTokenGet)
     Hippolyte.shared.add(stubbedRequest: createApiToken)
 
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
+    if let error = waitForError(client.login(email: "foo@example.com", password: "bar")) {
       guard let webClientError = error as? WaniKaniWebClientError else {
         XCTFail("Bad error type: " + error.localizedDescription)
         return
       }
       XCTAssertEqual(webClientError, WaniKaniWebClientError.apiTokenNotFound)
-    }
-  }
-
-  func testEmailNotFound() {
-    getEmailAddress.response.body = Data()
-    Hippolyte.shared.add(stubbedRequest: getLoginPage)
-    Hippolyte.shared.add(stubbedRequest: postLoginPage)
-    Hippolyte.shared.add(stubbedRequest: getDashboardPage)
-    Hippolyte.shared.add(stubbedRequest: getApiToken)
-    Hippolyte.shared.add(stubbedRequest: getEmailAddress)
-
-    if let error = waitForError(client.login(username: "foo", password: "bar")) {
-      guard let webClientError = error as? WaniKaniWebClientError else {
-        XCTFail("Bad error type: " + error.localizedDescription)
-        return
-      }
-      XCTAssertEqual(webClientError, WaniKaniWebClientError.emailNotFound)
     }
   }
 }
